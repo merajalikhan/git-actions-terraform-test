bed6ea0b6acd1ed2cb41a9847ddcdf6ec5740ca8
merajalikhan <104003533+merajalikhan@users.noreply.github.com>
2023-05-11 14:26:14 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,10 +4,6 @@ terraform {
       source = "databricks/databricks"
     }
   }
-  provider "databricks" {}
-    
-  data "databricks_current_user" "logged_in_user" {}
-  
   backend "remote" {
          # The name of your Terraform Cloud organization.
          organization = "mk-test-organisation"
@@ -16,6 +12,16 @@ terraform {
          workspaces {
            name = "learn-terraform-github-action"
          }
-       }
+  }
+  # An example resource that does nothing.
+  resource "null_resource" "example" {
+      triggers = {
+        value = "A example resource that does nothing!"
+      }
+  }
+  
+  provider "databricks" {}
+    
+  data "databricks_current_user" "logged_in_user" {}
 }
 
