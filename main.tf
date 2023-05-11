114bba2f32b8fd79d8651b4b809eb3a2f72eb269
merajalikhan <104003533+merajalikhan@users.noreply.github.com>
2023-05-11 14:28:09 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,22 +4,6 @@ terraform {
       source = "databricks/databricks"
     }
   }
-  backend "remote" {
-         # The name of your Terraform Cloud organization.
-         organization = "mk-test-organisation"
-
-         # The name of the Terraform Cloud workspace to store Terraform state files in.
-         workspaces {
-           name = "learn-terraform-github-action"
-         }
-  }
-  # An example resource that does nothing.
-  resource "null_resource" "example" {
-      triggers = {
-        value = "A example resource that does nothing!"
-      }
-  }
-  
   provider "databricks" {}
     
   data "databricks_current_user" "logged_in_user" {}
