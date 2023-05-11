67ee473711e05e97fd93276a75383e62f9925812
merajalikhan <merajalikhan@hotmail.com>
2023-05-11 13:26:23 +0100

    mk changes

--- main.tf
+++ main.tf
@@ -4,9 +4,21 @@ terraform {
       source = "databricks/databricks"
     }
   }
+  backend "remote" {
+         # The name of your Terraform Cloud organization.
+         organization = "mk-test-organisation"
+
+         # The name of the Terraform Cloud workspace to store Terraform state files in.
+         workspaces {
+           name = "learn-terraform-github-actions"
+         }
+       }
 }
 provider "databricks" {}
     
 data "databricks_current_user" "logged_in_user" {
     
 }
+
+
+ 
