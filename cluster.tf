782882523a609ed54fd7cb5b1839e25fac22eb6c
merajalikhan <merajalikhan@hotmail.com>
2023-05-15 12:39:49 +0100

    added cluster and changed the workspace secret values

--- cluster.tf
+++ cluster.tf
@@ -1,13 +1,20 @@
-/*
+
+data "databricks_spark_version" "latest_lts" {
+  long_term_support = true
+}
 resource "databricks_cluster" "shared_autoscaling" {
   cluster_name            = var.cluster_name
-  spark_version           = "12.2 LTS (includes Apache Spark 3.3.2, Scala 2.12)"
+  spark_version           = data.databricks_spark_version.latest_lts.id #"12.2 LTS (includes Apache Spark 3.3.2, Scala 2.12)"
   node_type_id            = "Standard_DS3_v2"
-  autotermination_minutes = 10  
-
-  autoscale {
-    min_workers = 0
-    max_workers = 1
-  }
+  autotermination_minutes = 10   
+  num_workers = 1
+  
 }
-*/
+
+
+output "cluster_id" {
+ value = databricks_cluster.shared_autoscaling.id
+}
+
+
+ 
--- job.tf
+++ job.tf
@@ -1,22 +1,17 @@
-variable "job_name" {
-  description = "Job to orchestrate notebooks"
-  type        = string
-  default     = "Demo Job"
-}
-
-
 resource "databricks_job" "this" {
   name = var.job_name
-  existing_cluster_id = "0426-161834-vhc628e8" #databricks_cluster.this.cluster_id
+  #existing_cluster_id =  var.existing_dev_cluster_id  #databricks_cluster.this.cluster_id
+  existing_cluster_id =  data.databricks_cluster.my_cluster.id  
   notebook_task {
     notebook_path = databricks_notebook.nb2.path
   }
   email_notifications {
-    on_success = [ data.databricks_current_user.logged_in_user.user_name ]
-    on_failure = [ data.databricks_current_user.logged_in_user.user_name ]
+    on_success = [var.notification_email]
+    on_failure = [var.notification_email]
+    on_start = [var.notification_email]
   }
   schedule {
-    quartz_cron_expression =  "0 30 17 * * ?" 
+    quartz_cron_expression =  "0 40 16 * * ?" 
     timezone_id = "Europe/London"
   }
 }
