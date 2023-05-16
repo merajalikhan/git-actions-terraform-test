d62ad041799f9a13a94e2e7251e9172373104fc4
merajalikhan <104003533+merajalikhan@users.noreply.github.com>
2023-05-16 13:05:21 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,14 +6,14 @@ terraform {
   }
 
   #specifies which Terraform Cloud workspaces to use for  accessing shared state and any environment variables.
-  cloud {
-    organization = "mk-test-organisation"
+#  cloud {
+#    organization = "mk-test-organisation"
 
-    workspaces {
-      name = "learn-terraform-github-action"
-    }
-  }
-}
+#    workspaces {
+#      name = "learn-terraform-github-action"
+#    }
+#  }
+#}
 
 provider "databricks" {
   
