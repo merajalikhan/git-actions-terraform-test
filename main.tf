367784acf9a1ced42659be0a1a136e0c31c09ceb
merajalikhan <104003533+merajalikhan@users.noreply.github.com>
2023-05-16 16:12:05 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,12 +6,18 @@ terraform {
   }
 
   #specifies which Terraform Cloud workspaces to use for  accessing shared state and any environment variables.
-  cloud {
-    organization = "mk-test-organisation"
+  #cloud {
+  #  organization = "mk-test-organisation"
 
-    workspaces {
-      name = "learn-terraform-github-action"
-    }
+  #  workspaces {
+  #    name = "learn-terraform-github-action"
+  #  }
+  #}
+  
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "mk-test-organisation"
+    workspaces { prefix = "git-actions-" }
   }
 }
 
